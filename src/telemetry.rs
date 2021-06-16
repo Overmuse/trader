ef37fc356424d79b2e559fa3b155c7b13cf0deee
Sebastian Rollen <seb@overmu.se>
2021-06-15 21:25:29 -0400

    format logs as json

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,11 +1,13 @@
-use tracing::{subscriber::set_global_default, Subscriber};
+use tracing::subscriber::{set_global_default, Subscriber};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, FmtSubscriber};
+use tracing_subscriber::{layer::SubscriberExt, Registry};
 
 pub fn get_subscriber() -> impl Subscriber + Send + Sync {
-    FmtSubscriber::builder()
-        .with_env_filter(EnvFilter::from_default_env())
-        .finish()
+    let formatting_layer = BunyanFormattingLayer::new("polygon-data-relay".into(), std::io::stdout);
+    Registry::default()
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
 }
 
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
