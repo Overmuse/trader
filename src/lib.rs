dfc9bdf34cf87cbe1c9f58a8d53283c0d7fe9c9c
Sebastian Rollen <seb@overmu.se>
2021-07-14 10:45:16 -0400

    retry sending requests with backoff on errors

--- src/lib.rs
+++ src/lib.rs
@@ -23,15 +23,15 @@ fn parse_message(msg: OwnedMessage) -> Result<OrderIntent> {
 }
 
 #[tracing::instrument(name = "Executing OrderIntent", skip(api, oi))]
-async fn execute_order(api: &Client, oi: OrderIntent) -> Result<Order> {
+async fn execute_order(api: &Client, oi: &OrderIntent) -> Result<Order> {
     info!("Submitting order intent: {:?}", &oi);
-    api.send(SubmitOrder(oi)).await.map_err(From::from)
+    api.send(SubmitOrder(oi.clone())).await.map_err(From::from)
 }
 
 #[tracing::instrument(name = "Received message", skip(api, msg))]
 async fn handle_message(api: &Client, msg: OwnedMessage) -> Result<Order> {
     let order_intent = parse_message(msg)?;
-    execute_order(api, order_intent).await
+    again::retry(|| execute_order(api, &order_intent)).await
 }
 
 pub async fn run(settings: Settings) -> Result<()> {
@@ -194,5 +194,75 @@ mod test {
         .unwrap();
 
         handle_message(&client, msg).await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn handles_trainsient_errors() {
+        let payload = r#"{"symbol":"AAPL","qty":"1.5","side":"buy","type":"limit","limit_price":"100","time_in_force":"gtc","extended_hours":false,"client_order_id":"TEST","order_class":"simple"}"#;
+        let msg = OwnedMessage::new(
+            Some(payload.as_bytes().to_vec()), // payload
+            None,                              // header
+            "test".into(),                     // topic
+            Timestamp::NotAvailable,           //timestamp
+            1,                                 // partition
+            0,                                 // offset
+            None,                              // headers
+        );
+
+        let m1 = mock("POST", "/orders")
+            .match_header("apca-api-key-id", "APCA_API_KEY_ID")
+            .match_header("apca-api-secret-key", "APCA_API_SECRET_KEY")
+            .match_body(payload)
+            .with_status(500)
+            .create();
+        let m2 = mock("POST", "/orders")
+            .match_header("apca-api-key-id", "APCA_API_KEY_ID")
+            .match_header("apca-api-secret-key", "APCA_API_SECRET_KEY")
+            .match_body(payload)
+            .with_body(
+                r#"{
+        	    "id": "904837e3-3b76-47ec-b432-046db621571b",
+        	    "client_order_id": "TEST",
+        	    "created_at": "2018-10-05T05:48:59Z",
+        	    "updated_at": "2018-10-05T05:48:59Z",
+        	    "submitted_at": "2018-10-05T05:48:59Z",
+        	    "filled_at": null,
+        	    "expired_at": null,
+        	    "canceled_at": null,
+        	    "failed_at": null,
+        	    "replaced_at": null,
+        	    "replaced_by": null,
+        	    "replaces": null,
+        	    "asset_id": "904837e3-3b76-47ec-b432-046db621571b",
+        	    "symbol": "AAPL",
+        	    "asset_class": "us_equity",
+                    "notional": null,
+        	    "qty": "1.5",
+        	    "filled_qty": "0",
+                    "filled_avg_price": null,
+        	    "type": "limit",
+        	    "side": "buy",
+        	    "time_in_force": "gtc",
+        	    "limit_price": "100.00",
+        	    "status": "new",
+        	    "extended_hours": false,
+        	    "legs": null,
+                    "trail_price": null,
+                    "trail_percent": null,
+                    "hwm": null
+        	}"#,
+            )
+            .create();
+
+        let client = Client::new(
+            mockito::server_url(),
+            "APCA_API_KEY_ID".to_string(),
+            "APCA_API_SECRET_KEY".to_string(),
+        )
+        .unwrap();
+
+        handle_message(&client, msg).await.unwrap();
+        m1.assert();
+        m2.assert();
     }
 }
