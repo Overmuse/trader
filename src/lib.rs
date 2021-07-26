a73214e35e20c742bea3de76441f1243121e9d74
Sebastian Rollen <seb@overmu.se>
2021-07-26 17:48:11 -0400

    switch to using trade-intents

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,60 @@
 use alpaca::{
     common::Order,
     orders::{OrderIntent, SubmitOrder},
-    Client,
+    Client, Side,
 };
 use anyhow::{anyhow, Result};
 use futures::StreamExt;
 use kafka_settings::consumer;
 use rdkafka::{message::OwnedMessage, Message};
 use tracing::{info, warn};
+use trading_base::TradeIntent;
 
 mod settings;
 pub use settings::Settings;
 pub mod telemetry;
 
-#[tracing::instrument(name = "Parsing message into OrderIntent", skip(msg))]
-fn parse_message(msg: OwnedMessage) -> Result<OrderIntent> {
+#[tracing::instrument(name = "Parsing message into TradeIntent", skip(msg))]
+fn parse_message(msg: OwnedMessage) -> Result<TradeIntent> {
     match msg.payload_view::<str>() {
         Some(Ok(payload)) => serde_json::from_str(payload).map_err(From::from),
         Some(Err(e)) => Err(e.into()),
         None => Err(anyhow!("Empty message received")),
     }
+}
+
+fn translate_intent(ti: TradeIntent) -> OrderIntent {
+    let (qty, side) = if ti.qty > 0 {
+        (ti.qty as usize, Side::Buy)
+    } else {
+        (ti.qty.abs() as usize, Side::Sell)
+    };
+    let order_type = match ti.order_type {
+        trading_base::OrderType::Market => alpaca::OrderType::Market,
+        trading_base::OrderType::Limit { limit_price } => alpaca::OrderType::Limit { limit_price },
+        trading_base::OrderType::Stop { stop_price } => alpaca::OrderType::Stop { stop_price },
+        trading_base::OrderType::StopLimit {
+            stop_price,
+            limit_price,
+        } => alpaca::OrderType::StopLimit {
+            stop_price,
+            limit_price,
+        },
+    };
+    let time_in_force = match ti.time_in_force {
+        trading_base::TimeInForce::GoodTilCanceled => alpaca::TimeInForce::GoodTilCancelled,
+        trading_base::TimeInForce::Day => alpaca::TimeInForce::Day,
+        trading_base::TimeInForce::ImmediateOrCancel => alpaca::TimeInForce::ImmediateOrCancel,
+        trading_base::TimeInForce::FillOrKill => alpaca::TimeInForce::FillOrKill,
+        trading_base::TimeInForce::Open => alpaca::TimeInForce::Open,
+        trading_base::TimeInForce::Close => alpaca::TimeInForce::Close,
+    };
+    OrderIntent::new(&ti.ticker)
+        .qty(qty)
+        .side(side)
+        .order_type(order_type)
+        .time_in_force(time_in_force)
+        .client_order_id(ti.id.to_string())
 }
 
 #[tracing::instrument(name = "Executing OrderIntent", skip(api, oi))]
@@ -30,7 +65,8 @@ async fn execute_order(api: &Client, oi: &OrderIntent) -> Result<Order> {
 
 #[tracing::instrument(name = "Received message", skip(api, msg))]
 async fn handle_message(api: &Client, msg: OwnedMessage) -> Result<Order> {
-    let order_intent = parse_message(msg)?;
+    let trade_intent = parse_message(msg)?;
+    let order_intent = translate_intent(trade_intent);
     again::retry(|| execute_order(api, &order_intent)).await
 }
 
@@ -70,25 +106,12 @@ mod test {
     #[test]
     fn unwrap_msg() {
         let payload = r#"{
-            "symbol":"AAPL",
-            "qty":"1.0",
-            "side":"buy",
-            "type":"limit",
+            "ticker":"AAPL",
+            "qty":1,
+            "order_type":"limit",
             "limit_price":"100",
             "time_in_force":"gtc",
-            "extended_hours":false,
-            "client_order_id":"TEST",
-            "order_class":{
-                "bracket":{
-                    "take_profit":{
-                        "limit_price":301.0
-                    },
-                    "stop_loss":{
-                        "stop_price":299.0,
-                        "limit_price":298.5
-                    }
-                }
-            }
+            "id":"904837e3-3b76-47ec-b432-046db621571b"
         }"#
         .as_bytes()
         .to_vec();
@@ -101,8 +124,8 @@ mod test {
             0,    // offset
             None, // headers
         );
-        let oi = parse_message(msg).unwrap();
-        assert_eq!(oi.symbol, "AAPL".to_string());
+        let ti = parse_message(msg).unwrap();
+        assert_eq!(&ti.ticker, "AAPL");
     }
 
     #[test]
@@ -137,7 +160,8 @@ mod test {
 
     #[tokio::test]
     async fn generates_correct_trade() {
-        let payload = r#"{"symbol":"AAPL","qty":"1.5","side":"buy","type":"limit","limit_price":"100","time_in_force":"gtc","extended_hours":false,"client_order_id":"TEST","order_class":"simple"}"#;
+        let payload = r#"{"ticker":"AAPL","qty":1,"order_type":"limit","limit_price":100,"time_in_force":"gtc","id":"904837e3-3b76-47ec-b432-046db621571b"}"#;
+        let order_payload = r#"{"symbol":"AAPL","qty":"1","side":"buy","type":"limit","limit_price":"100","time_in_force":"gtc","extended_hours":false,"client_order_id":"904837e3-3b76-47ec-b432-046db621571b","order_class":"simple"}"#;
         let msg = OwnedMessage::new(
             Some(payload.as_bytes().to_vec()), // payload
             None,                              // header
@@ -151,11 +175,11 @@ mod test {
         let _m = mock("POST", "/orders")
             .match_header("apca-api-key-id", "APCA_API_KEY_ID")
             .match_header("apca-api-secret-key", "APCA_API_SECRET_KEY")
-            .match_body(payload)
+            .match_body(order_payload)
             .with_body(
                 r#"{
 		    "id": "904837e3-3b76-47ec-b432-046db621571b",
-		    "client_order_id": "TEST",
+		    "client_order_id": "904837e3-3b76-47ec-b432-046db621571b",
 		    "created_at": "2018-10-05T05:48:59Z",
 		    "updated_at": "2018-10-05T05:48:59Z",
 		    "submitted_at": "2018-10-05T05:48:59Z",
@@ -169,10 +193,10 @@ mod test {
 		    "asset_id": "904837e3-3b76-47ec-b432-046db621571b",
 		    "symbol": "AAPL",
 		    "asset_class": "us_equity",
-                    "notional": null,
-		    "qty": "1.5",
+            "notional": null,
+		    "qty": "1",
 		    "filled_qty": "0",
-                    "filled_avg_price": null,
+            "filled_avg_price": null,
 		    "type": "limit",
 		    "side": "buy",
 		    "time_in_force": "gtc",
@@ -180,9 +204,9 @@ mod test {
 		    "status": "new",
 		    "extended_hours": false,
 		    "legs": null,
-                    "trail_price": null,
-                    "trail_percent": null,
-                    "hwm": null
+            "trail_price": null,
+            "trail_percent": null,
+            "hwm": null
 		}"#,
             )
             .create();
@@ -198,7 +222,8 @@ mod test {
 
     #[tokio::test]
     async fn handles_trainsient_errors() {
-        let payload = r#"{"symbol":"AAPL","qty":"1.5","side":"buy","type":"limit","limit_price":"100","time_in_force":"gtc","extended_hours":false,"client_order_id":"TEST","order_class":"simple"}"#;
+        let payload = r#"{"ticker":"AAPL","qty":1,"order_type":"limit","limit_price":"100","time_in_force":"gtc","id":"904837e3-3b76-47ec-b432-046db621571b"}"#;
+        let order_payload = r#"{"symbol":"AAPL","qty":"1","side":"buy","type":"limit","limit_price":"100","time_in_force":"gtc","extended_hours":false,"client_order_id":"904837e3-3b76-47ec-b432-046db621571b","order_class":"simple"}"#;
         let msg = OwnedMessage::new(
             Some(payload.as_bytes().to_vec()), // payload
             None,                              // header
@@ -212,17 +237,17 @@ mod test {
         let m1 = mock("POST", "/orders")
             .match_header("apca-api-key-id", "APCA_API_KEY_ID")
             .match_header("apca-api-secret-key", "APCA_API_SECRET_KEY")
-            .match_body(payload)
+            .match_body(order_payload)
             .with_status(500)
             .create();
         let m2 = mock("POST", "/orders")
             .match_header("apca-api-key-id", "APCA_API_KEY_ID")
             .match_header("apca-api-secret-key", "APCA_API_SECRET_KEY")
-            .match_body(payload)
+            .match_body(order_payload)
             .with_body(
                 r#"{
         	    "id": "904837e3-3b76-47ec-b432-046db621571b",
-        	    "client_order_id": "TEST",
+        	    "client_order_id": "904837e3-3b76-47ec-b432-046db621571b",
         	    "created_at": "2018-10-05T05:48:59Z",
         	    "updated_at": "2018-10-05T05:48:59Z",
         	    "submitted_at": "2018-10-05T05:48:59Z",
@@ -236,10 +261,10 @@ mod test {
         	    "asset_id": "904837e3-3b76-47ec-b432-046db621571b",
         	    "symbol": "AAPL",
         	    "asset_class": "us_equity",
-                    "notional": null,
-        	    "qty": "1.5",
+                "notional": null,
+        	    "qty": "1",
         	    "filled_qty": "0",
-                    "filled_avg_price": null,
+                "filled_avg_price": null,
         	    "type": "limit",
         	    "side": "buy",
         	    "time_in_force": "gtc",
@@ -247,9 +272,9 @@ mod test {
         	    "status": "new",
         	    "extended_hours": false,
         	    "legs": null,
-                    "trail_price": null,
-                    "trail_percent": null,
-                    "hwm": null
+                "trail_price": null,
+                "trail_percent": null,
+                "hwm": null
         	}"#,
             )
             .create();
